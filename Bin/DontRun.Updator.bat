2bddd8a39ca28195dc0ef26b4de0620ec1cb7ec7
Lucas Elliott <Lucas_github@itcommand.net>
2022-10-03 09:03:10 -0400

    Update DontRun.Updator.bat

--- Bin/DontRun.Updator.bat
+++ Bin/DontRun.Updator.bat
@@ -2,9 +2,9 @@
 cls
 echo [96mDownloading files...[90m
 echo Downloading Simple-Radio-COM.bat...
-curl https://raw.githubusercontent.com/ITCMD/SRCOM/main/Simple-Radio-COM.bat -s --progressbar -o ..\Simple-Radio-COM.bat
+curl https://raw.githubusercontent.com/ITCMD/SRCOM/main/Simple-Radio-COM.bat -s --progress-bar -o ..\Simple-Radio-COM.bat
 echo Downloading Logger.cmd...
-curl https://raw.githubusercontent.com/ITCMD/SRLog/main/Simple-Radio-Logger.bat -s --progressbar -o Logger.bat
+curl https://raw.githubusercontent.com/ITCMD/SRLog/main/Simple-Radio-Logger.bat -s --progress-bar -o Logger.bat
 echo.
 echo [92mDone![0m
 pause
